105d481585e4ac18d10d5082be709f2e0bf69fce
sheymantech <olaniyansheyi1704@gmail.com>
2024-03-03 22:07:54 +0100

    new feature

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,3 +1,12 @@
 export default function StarRating() {
-    return <div>hello</div>
+  return (
+    <div>
+      <div>
+        {Array.from({ length: 5 }, (_, i) => (
+          <span>S{1 + i}</span>
+        ))}
+      </div>
+      <p>10</p>
+    </div>
+  );
 }
