8a3d1944046136f302429dd84e951d32e41d5a55
sheymantech <olaniyansheyi1704@gmail.com>
2024-03-04 15:50:18 +0100

    new feature

--- src/index.js
+++ src/index.js
@@ -9,6 +9,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={10} />
+    <StarRating maxRating={5} />
   </React.StrictMode>
 );
