a101e86bbe96624e39ec1ed332980a6aa49fe5c1
sheymantech <olaniyansheyi1704@gmail.com>
2024-03-06 16:32:38 +0100

    new feature

--- src/StarRating.js
+++ src/StarRating.js
@@ -10,15 +10,11 @@ const starContainerStyle = {
   display: "flex",
 };
 
-const textStyle = {
-  lineHieght: "1px",
-  margin: "0",
-};
-
 export default function StarRating({
   maxRating = 5,
   color = "#fcc419",
   size = 48,
+  messages = [],
 }) {
   const [rating, setRating] = useState(4);
   const [tempRating, setTempRating] = useState(0);
@@ -48,7 +44,11 @@ export default function StarRating({
           />
         ))}
       </div>
-      <p style={textStyle}>{tempRating || rating || ""}</p>
+      <p style={textStyle}>
+        {messages.length === maxRating
+          ? messages[tempRating ? tempRating - 1 : rating - 1]
+          : tempRating || rating || ""}
+      </p>
     </div>
   );
 }
@@ -102,24 +102,3 @@ function Star({ onRate, full, onHoverIn, onHoverOut, color, size }) {
     </span>
   );
 }
-
-/*
-FULL STAR
-
-<svg
-  xmlns="http://www.w3.org/2000/svg"
-  viewBox="0 0 20 20"
-  fill="#000"
-  stroke="#000"
->
-  <path
-    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-  />
-</svg>
-
-
-EMPTY STAR
-
-
-
-*/
